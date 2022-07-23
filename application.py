8a07f93c50d522de97f885953fb345f2a34d3a8c
Shavarsh Shahoyan <shavarsh.sh@gmail.com>
2022-07-23 14:10:20 +0400

    added json formatting to return text as a json

--- application.py
+++ application.py
@@ -1,15 +1,18 @@
 from youtube_transcript_api import YouTubeTranscriptApi
+from youtube_transcript_api.formatters import JSONFormatter
 
 from flask import Flask
 application = Flask(__name__)
 
 @application.route('/', methods=['GET'])
 def hello():
-    return "hello"
+    return "wordbook"
 
 @application.route('/getYoutubeVideoTextByID/<string:ID>', methods=['GET'])
 def welcome(ID):
     listOfTexts= YouTubeTranscriptApi.get_transcript(ID)
-    return ''.join(str(e) for e in listOfTexts)
+    formatter = JSONFormatter()
+    json_formatted = formatter.format_transcript(listOfTexts)
+    return json_formatted    
 if __name__ == '__main__':
     application.run(host='0.0.0.0', port=5050)
